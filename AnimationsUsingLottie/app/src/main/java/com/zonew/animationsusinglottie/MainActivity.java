2218b2e31a67e730446e0e8426e0e725d824e46b
Nitikarn <nitikran12@gmail.com>
2021-06-04 13:07:41 +0700

    Success

--- AnimationsUsingLottie/app/src/main/java/com/zonew/animationsusinglottie/MainActivity.java
+++ AnimationsUsingLottie/app/src/main/java/com/zonew/animationsusinglottie/MainActivity.java
@@ -3,12 +3,53 @@ package com.zonew.animationsusinglottie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isCheckedDone = false;
+    boolean isSwitchOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final LottieAnimationView lottieCheckedDone = findViewById(R.id.lottieCheckedDone);
+        lottieCheckedDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int speed = 3;
+
+                if (isCheckedDone) {
+                    lottieCheckedDone.setSpeed(-speed); // reverse
+                    lottieCheckedDone.playAnimation();
+                    isCheckedDone = false;
+                }
+
+                else {
+                    lottieCheckedDone.setSpeed(speed); // regular
+                    lottieCheckedDone.playAnimation();
+                    isCheckedDone = true;
+                }
+            }
+        });
+        LottieAnimationView lottieSwitchButton = findViewById(R.id.lottieSwitchButton);
+        lottieSwitchButton.setSpeed(3f);
+        lottieSwitchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isSwitchOn) {
+                    lottieSwitchButton.setMinAndMaxProgress(0.5f, 1.0f);
+                    lottieSwitchButton.playAnimation();
+                    isSwitchOn = false;
+                } else {
+                    lottieSwitchButton.setMinAndMaxProgress(0.0f, 0.5f);
+                    lottieSwitchButton.playAnimation();
+                    isSwitchOn = true;
+                }
+            }
+        });
     }
 }
