93bb843891869dc8152ec65153402b783e77b54d
ZiXAX <nitikran12@gmail.com>
2021-06-17 22:14:36 +0700

    add comment

--- AnimationsUsingLottie/app/src/main/java/com/zonew/animationsusinglottie/MainActivity.java
+++ AnimationsUsingLottie/app/src/main/java/com/zonew/animationsusinglottie/MainActivity.java
@@ -16,18 +16,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Checked
         final LottieAnimationView lottieCheckedDone = findViewById(R.id.lottieCheckedDone);
         lottieCheckedDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 int speed = 3;
 
+                // on
                 if (isCheckedDone) {
                     lottieCheckedDone.setSpeed(-speed); // reverse
                     lottieCheckedDone.playAnimation();
                     isCheckedDone = false;
                 }
-
+                // off
                 else {
                     lottieCheckedDone.setSpeed(speed); // regular
                     lottieCheckedDone.playAnimation();
@@ -35,16 +37,22 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        // Switch button
         LottieAnimationView lottieSwitchButton = findViewById(R.id.lottieSwitchButton);
         lottieSwitchButton.setSpeed(3f);
         lottieSwitchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                // on
                 if (isSwitchOn) {
                     lottieSwitchButton.setMinAndMaxProgress(0.5f, 1.0f);
                     lottieSwitchButton.playAnimation();
                     isSwitchOn = false;
-                } else {
+                }
+                // off
+                else {
                     lottieSwitchButton.setMinAndMaxProgress(0.0f, 0.5f);
                     lottieSwitchButton.playAnimation();
                     isSwitchOn = true;
