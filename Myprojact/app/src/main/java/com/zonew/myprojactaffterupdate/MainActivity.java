1bdbfd24530f960bc8bcd1a98ed820967c614b97
ZoNew <nitikran12@gmail.com>
2021-06-27 00:16:55 +0700

    Change UI ID

--- Myprojact/app/src/main/java/com/zonew/myprojactaffterupdate/MainActivity.java
+++ Myprojact/app/src/main/java/com/zonew/myprojactaffterupdate/MainActivity.java
@@ -1,9 +1,6 @@
 package com.zonew.myprojactaffterupdate;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -30,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_member, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
 //        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
